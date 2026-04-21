94767dd49befa9ba941ee1a8e95ac5c16203fe64
Henré Botha <5593874+henrebotha@users.noreply.github.com>
2026-04-21 10:17:27 +0200

    Improve index formatting

--- asciidoc/extensions/generate-index.rb
+++ asciidoc/extensions/generate-index.rb
@@ -2,9 +2,11 @@ require 'asciidoctor/extensions'
 
 def find_posts
   Dir.glob('src/posts/*.adoc')
-    .map { |p| p.gsub(/$/, '[' + File.open(p, &:readline).gsub(/^= /, '').strip + ']') }
-    .map { |p| p.gsub(/^src\//, '') }
-    .map { |p| 'xref:' + p }
+    .reverse.map.with_index do |p, i|
+      title = File.open(p, &:readline).gsub(/^= /, '').strip
+      date = p.gsub(/^.*(\d{4}-\d{2}-\d{2}).*$/, '\1')
+      date + (i == 0 ? ' (latest): ' : ': ') + 'xref:' + p.gsub(/$/, '[' + title + ']').gsub(/^src\//, '')
+    end.reverse
 end
 
 Asciidoctor::Extensions.register do
@@ -13,7 +15,8 @@ Asciidoctor::Extensions.register do
       if doc.attr? 'generate-index'
         posts = find_posts()
 
-        list = Asciidoctor::List.new(doc, :ulist, attributes: { 'role' => 'unstyled' })
+        list = Asciidoctor::List.new(doc, :ulist, attributes: { 'role' => 'tnum' })
+        list.style = 'no-bullet'
         posts.each { |p| list << Asciidoctor::ListItem.new(list, p) }
 
         doc.blocks << list
