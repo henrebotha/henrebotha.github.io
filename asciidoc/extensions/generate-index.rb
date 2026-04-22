88e2883cf23544b873c0f5048bd069ef79d8cb67
Henré Botha <5593874+henrebotha@users.noreply.github.com>
2026-04-22 10:24:54 +0200

    Add debugging tools

--- asciidoc/extensions/generate-index.rb
+++ asciidoc/extensions/generate-index.rb
@@ -2,7 +2,8 @@ require 'asciidoctor/extensions'
 
 def find_posts
   Dir.glob('src/posts/*.adoc')
-    .reverse.map.with_index do |p, i|
+    .reverse
+    .map.with_index do |p, i|
       title = File.open(p, &:readline).gsub(/^= /, '').strip
       date = p.gsub(/^.*(\d{4}-\d{2}-\d{2}).*$/, '\1')
       date + (i == 0 ? ' (latest): ' : ': ') + 'xref:' + p.gsub(/$/, '[' + title + ']').gsub(/^src\//, '')
@@ -12,7 +13,10 @@ end
 Asciidoctor::Extensions.register do
   tree_processor do
     process do |doc|
+      singleton_class.include Asciidoctor::Logging
+
       if doc.attr? 'generate-index'
+        logger.info "Generating index for #{doc}"
         posts = find_posts()
 
         list = Asciidoctor::List.new(doc, :ulist, attributes: { 'role' => 'tnum' })
@@ -20,9 +24,6 @@ Asciidoctor::Extensions.register do
         posts.each { |p| list << Asciidoctor::ListItem.new(list, p) }
 
         doc.blocks << list
-        # [unstyled]
-        # * xref:posts/2026-03-19-exquis-setup.adoc[Exquis setup]
-        # * xref:posts/2015-07-01-ruby-variable-initialisation.adoc[Ruby variable initialisation]
       end
     end
   end
