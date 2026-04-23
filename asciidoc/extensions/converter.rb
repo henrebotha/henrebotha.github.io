a68823a9ad62b25b97eadd4b620229f7f35eb526
Henré Botha <5593874+henrebotha@users.noreply.github.com>
2026-04-23 14:11:08 +0200

    Handle conditionals in Slim

--- asciidoc/extensions/converter.rb
+++ asciidoc/extensions/converter.rb
@@ -13,30 +13,15 @@ class MyHtml5Converter < (Asciidoctor::Converter.for 'html5')
     unless doc.attributes['docdir'].match(/\b_include\b/)
       processed = render_header(doc, processed)
     end
-
-    doc.attr?('date') ? insert_time(processed, doc.attr('date')) : processed
   end
 
   private
 
   def render_header(doc, processed)
-    template = Tilt.new('asciidoc/templates/docinfo-header.html.slim').render(self, doctitle: doc.title)
+    template = Tilt.new('asciidoc/templates/docinfo-header.html.slim')
+      .render(self, doctitle: doc.title, date: doc.attributes['date'])
 
     parts = processed.partition(/<body[^>]*>/)
     processed = parts[0] + parts[1] + template + parts[2]
-  end
-
-  def insert_time(processed, date)
-    header_regex = %r{(<header[^>]*>)(.*)</header>}m
-    has_header = processed.match(header_regex)
-
-    return processed unless has_header
-
-    header = has_header[0]
-    header_element = has_header[1]
-    header_content = has_header[2]
-    new_header = %(#{header_element}#{header_content}<time class="posted-at">#{date}</time></header>)
-
-    processed.gsub(header, new_header)
   end
 end
